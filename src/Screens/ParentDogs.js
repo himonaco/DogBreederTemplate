c9dd099ebd4ac7ec58361d94ff82b7aa76728d89
Luca Monaco <lucamonaco@icloud.com>
2024-05-21 22:06:27 +0200

    started the subpages

--- src/Screens/ParentDogs.js
+++ src/Screens/ParentDogs.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ResponsiveAppBar from '../Components/ResponsiveAppBar';
 import MyFooter from '../Components/MyFooter';
+import ActionAreaCard from '../Components/ActionAreaCard';
+import './ParentDogs.css'
 
 export default function ParentDogs() {
   return (
     <div>
       <ResponsiveAppBar />
-      <h1>Welcome to the Parent Dogs Page</h1>
+      <div className='content'>
+        <h1>Welcome to the Parent Dogs Page</h1>
+      <div className='cards'>
+        <ActionAreaCard className="card" />
+        <ActionAreaCard className="card" />
+        <ActionAreaCard className="card" />
+        <ActionAreaCard className="card" />
+        <ActionAreaCard className="card" />
+        <ActionAreaCard className="card" />
+      </div>
+      </div>
       <MyFooter />
     </div>
   )
